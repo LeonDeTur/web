a4b6469c96a5a8edb295037abaaa9d5cd1629883
Leon Tur <tutrkov.leon-2@yandex.ru>
2024-01-16 22:53:29 +0300

    Added files

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,31 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pm%n2%h#al3!!tlocv2$01s-t*49d@$!!%g2r4%!7l#q&u3qk7'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+ALLOWED_HOSTS = ['127.0.0.1', 'LeonDeTur.pythonanywhere.com',]
+
+STATIC_ROOT = BASE_DIR / 'static/'
+
+DATABASES = {
+'default': {
+'ENGINE': 'django.db.backends.mysql',
+'NAME': 'LeonDeTur$default',
+'USER': 'LeonDeTur',
+'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+'HOST': 'LeonDeTur.mysql.pythonanywhere-services.com',
+'OPTIONS': {
+'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+'charset': 'utf8mb4',
+},
+}
+}
 
 
 # Application definition
